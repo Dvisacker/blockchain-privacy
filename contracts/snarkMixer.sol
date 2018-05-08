e0a673c274f96319b9c5442368a3cb8cd295072b
Antoine Rondelet <rondelet.antoine@gmail.com>
2018-05-08 20:54:22 +0100

    First version of a naive mixer

--- contracts/snarkMixer.sol
+++ contracts/snarkMixer.sol
@@ -1,40 +1,35 @@
 pragma solidity ^0.4.19;
 
-contract SnarkPrecompile {
-    function verify_proof(bytes, bytes, bytes) returns (bool);
-}
+import "./verifier.sol";
 
-contract Mixer {
-    mapping (bytes32 => bool) public serials;
+contract Mixer is Verifier {
     mapping (bytes32 => bool) public roots;
-    SnarkPrecompile zksnark = SnarkPrecompile(0x0000000000000000000000000000000000000005);
 
     struct MerkleTree {
         uint currentLeafIndex;
-        bytes32[16] leaves;
+        uint[16] leaves;
     }
 
     MerkleTree public tree;
-    bytes public verificationKey;
 
-    function Mixer(bytes _verificationKey) {
-        verificationKey = _verificationKey;
+    function Mixer() {
         tree.currentLeafIndex = 0;
-        for (uint i = 0; i < 16; i++)
+        for (uint i = 0; i < 16; i++) {
             tree.leaves[i] = 0x0;
+        }
     }
 
     // MerkleTree.append(commitment)
-    function insert(bytes32 commitment) returns (bool res) {
+    function insert(uint numberToFactorize) returns (bool res) {
         if (tree.currentLeafIndex == 16) {
             return false;
         }
-        tree.leaves[tree.currentLeafIndex] = commitment;
+        tree.leaves[tree.currentLeafIndex] = numberToFactorize;
         tree.currentLeafIndex++;
         return true;
     }
 
-    function getLeaves() constant returns (bytes32[16]) {
+    function getLeaves() constant returns (uint[16]) {
         return tree.leaves;
     }
 
@@ -42,7 +37,7 @@ contract Mixer {
         // bytes32[32] memory currentTree;
         uint i;
         for (i = 0; i < 16; i++) {
-            currentTree[16 + i] = tree.leaves[i];
+            currentTree[16 + i] = sha256(tree.leaves[i]);
         }
 
         for (i = 16 - 1; i > 0; i--) {
@@ -56,13 +51,25 @@ contract Mixer {
     function getRoot() constant returns(bytes32 root) {
         root = getTree()[1];
     }
+    
+    function isElementOfArray(uint element, uint[16] array) returns (bool) {
+        bool find = false;
+        uint arrayLength = array.length;
 
-    function deposit(bytes32 commitment) returns (bool res) {
+        for (uint i=0; i < arrayLength; i++) {
+            if (array[i] == element) {
+                find = true;
+            }
+        }
+        return find;
+    }
+
+    function deposit(uint numberToFactorize) returns (bool res) {
         if (msg.value != 1 ether) {
             msg.sender.send(msg.value);
             return false;
         }
-        if (!insert(commitment)) {
+        if (!insert(numberToFactorize)) {
             msg.sender.send(msg.value);
             return false;
         }
@@ -71,48 +78,32 @@ contract Mixer {
         return true;
     }
 
-    function withdraw(bytes32 serial, address addr, bytes32 rootTree, bytes32 mac, bytes proof) returns (bool success) {
-        success = false;
-        bytes20 addr_byte = bytes20(addr);
-        bytes memory pub = new bytes(128);
-        uint i;
-
-        for (i = 0; i < 32; i++) {
-            pub[i] = serial[i];
-        }
-
-        for (i = 0; i < 20; i++) {
-            pub[32 + i] = addr_byte[i];
-        }
-
-        for (i = 20; i < 32; i++) {
-            pub[32 + i] = 0;
-        }
-
-        for (i = 0; i < 32; i++) {
-            pub[32*2 + i] = rootTree[i];
-        }
-
-        for (i = 0; i < 32; i++) {
-            pub[32*3 + i] = mac[i];
-        }
-
-        if (roots[rootTree] == true) {
-            if (!serials[serial]) {
-                if (!zksnark.verify_proof(verificationKey, proof, pub)) {
-                    return false;
-                }
-                serials[serial] = true;
-                if (!addr.send(1 ether)) {
-                    throw;
-                } else {
-                    success = true;
-                }
+    function withdraw(
+        bytes32 rootTree,
+        uint[2] a,
+        uint[2] a_p,
+        uint[2][2] b,
+        uint[2] b_p,
+        uint[2] c,
+        uint[2] c_p,
+        uint[2] h,
+        uint[2] k,
+        uint[2] input
+    ) returns (bool) {
+        uint[16] memory leaves = getLeaves();
+        // Verify that we provide a proof for an element in the array
+        bool find = isElementOfArray(input[1], leaves);
+        if (roots[rootTree] == true && find) {
+            if (!verifyTx(a, a_p, b, b_p, c, c_p, h, k, input)) {
+                return false;
+            }
+            if (!msg.sender.send(1 ether)) {
+                throw;
             } else {
-                return;
+                return true;
             }
         } else {
-            return;
+             return false;
         }
     }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,15 @@
 //const KeyManager = artifacts.require("./KeyManager.sol");
 //const TransactionRelay = artifacts.require("./TransactionRelay.sol");
 //const CoinProvider = artifacts.require("./CoinProvider.sol");
-const Verifier = artifacts.require("./Verifier.sol");
-const Factorize = artifacts.require("./factorize.sol");
+const SnarkMixer = artifacts.require("./snarkMixer.sol");
+const Verifier = artifacts.require("./verifier.sol");
+//const Factorize = artifacts.require("./factorize.sol");
 
 module.exports = (deployer) => {
   //deployer.deploy(KeyManager);
   //deployer.deploy(TransactionRelay);
   //deployer.deploy(CoinProvider);
+  deployer.deploy(SnarkMixer);
   deployer.deploy(Verifier);
-  deployer.deploy(Factorize);
+  //deployer.deploy(Factorize);
 };
